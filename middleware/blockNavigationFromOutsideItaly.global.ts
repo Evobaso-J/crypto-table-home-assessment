29bbca309f87286584a4c91560fed43b82a398a3
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-05 16:40:47 +0200

    fix: temp remove middleware

--- middleware/blockNavigationFromOutsideItaly.global.ts
+++ middleware/blockNavigationFromOutsideItaly.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware(() => {
-  const { data } = useFetch('http://ip-api.com/json', {
+  const { data: _ } = useFetch('http://ip-api.com/json', {
     method: 'GET',
     query: {
       fields: 'countryCode',
     },
   })
-  if (data.value !== 'IT') {
-    return abortNavigation('Devi essere in Italia per accedere a questa pagina')
-  }
+  // if (data.value !== 'IT') {
+  //   return abortNavigation('Devi essere in Italia per accedere a questa pagina')
+  // }
 })
