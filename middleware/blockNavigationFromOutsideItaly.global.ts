fd1c0875795ef0539511f67be3a4e19e5f5b335e
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-05 16:38:05 +0200

    fix: bug in middleware

--- middleware/blockNavigationFromOutsideItaly.global.ts
+++ middleware/blockNavigationFromOutsideItaly.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware(() => {
-  const { data } = useFetch('http://ip-api.com/json/24.48.0.1', {
+  const { data } = useFetch('http://ip-api.com/json', {
     method: 'GET',
     query: {
       fields: 'countryCode',
     },
   })
-  if (data.value !== 'SE') {
+  if (data.value !== 'IT') {
     return abortNavigation('Devi essere in Italia per accedere a questa pagina')
   }
 })
